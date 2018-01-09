8e58d0bbe8f95d28ce1725130b252859e9eb6342
Thrisiser <35246335+Thrisiser@users.noreply.github.com>
2018-01-09 13:59:52 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -1,5 +1,5 @@
 <?php
- echo "Hello, I am Captain America!\n";
+ echo "Hello, I am Captain America!<br>";
  echo "Send me a Line Message!";
  $json_str = file_get_contents('php://input'); //接收REQUEST的BODY
  $json_obj = json_decode($json_str); //轉JSON格式
@@ -16,14 +16,15 @@
 					)
 				)
 		);
-
+ 
+ //寫入日誌檔
  $myfile = fopen("log.txt","w+") or die("Unable to open file!"); //設定一個log.txt 用來印訊息
  fwrite($myfile, "\xEF\xBB\xBF".$json_str); //在字串前加入\xEF\xBB\xBF轉成utf8格式 大小寫有差
  fclose($myfile);
 
  //回傳給line server
 	$header[] = "Content-Type: application/json";
-	$header[] = "Authorization: Bearer 2dlO4f5JCjLNWypaXTnItbtxhIVQLKAGvPv1GS3aNUxYs7MUKu5jtsRzAO+TqN2G6ksPVi+KForszbV4NXXTSLBnchyaZ4wTPP7OgLfeTXgXz1YjlD1G3RC/iMwSmmVocmFnqG8QHrcm1v/ASkKbqQdB04t89/1O/w1cDnyilFU=";
+	$header[] = "Authorization: Bearer 2dlO4f5JCjLNWypaXTnItbtxhIVQLKAGvPv1GS3aNUxYs7MUKu5jtsRzAO+TqN2G6ksPVi+KForszbV4NXXTSLBnchyaZ4wTPP7OgLfeTXgXz1YjlD1G3RC/iMwSmmVocmFnqG8QHrcm1v/ASkKbqQdB04t89/1O/w1cDnyilFU="; //這裡要改成自己的Channel access token
 	$ch = curl_init("https://api.line.me/v2/bot/message/push");                                                                      
 	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
 	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));                                                                  
